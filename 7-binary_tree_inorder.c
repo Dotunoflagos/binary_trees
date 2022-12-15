26a064c0c1eacf90e9376bc757752fd54e8146e4
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-15 05:03:36 +0100

    binary trees

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - traverses a tree with .
+ * binary_tree_inorder - traverses a tree with .
  * @tree: root node to begin traversal at.
  * @func: function to call 
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
 
-	binary_tree_preorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 	func(tree->n);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - traverses a tree with .
+ * binary_tree_postorder - traverses a tree with .
  * @tree: root node to begin traversal at.
  * @func: function to call 
  */
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
 
-	binary_tree_preorder(tree->left, func);
-	binary_tree_preorder(tree->right, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
