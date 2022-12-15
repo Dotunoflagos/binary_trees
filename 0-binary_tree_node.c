998867cb351ff88d775e6126f404f12f55fe5081
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-15 04:03:29 +0100

    binary trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,11 +10,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (new_node == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL || parent == NULL || value == NULL)
 	{
 		return (NULL);
 	}
-    new_node = malloc(sizeof(binary_tree_t));
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
