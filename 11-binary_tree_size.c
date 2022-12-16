1419b15963196ba65c007e989464aa8c199e1711
dotunoflagos <adedotun_balogun@yahoo.com>
2022-12-16 23:21:15 +0100

    binary trees

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -5,7 +5,7 @@
  * @tree: pointer node binary tree
  * Return: measures the size
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t depthr = 0;
 	size_t depthl = 0;
@@ -14,8 +14,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		depthr += 1 + binary_tree_depth(tree->left);
+		depthr += 1 + binary_tree_size(tree->left);
     if (tree->right)
-		depthl += 1 + binary_tree_depth(tree->right);
+		depthl += 1 + binary_tree_size(tree->right);
 	return (depthr + depthl);
 }
